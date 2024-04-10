1dbd8b3a068681dff49e208939a737e54dc2cb0e
kendrick <kendrickloi123@gmail.com>
2024-04-10 14:57:32 +0800

    minor edits TODO: add volumes to docker-compose

--- backend testing/app.py
+++ backend testing/app.py
@@ -7,41 +7,37 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 # Define a Flask route to handle POST requests
-@app.route('/initialise_survey', methods=['POST'])
+@app.route("/initialise_survey", methods=["POST"])
 def initialise_survey():
-    
+
     # Call on the function to start the survey and get LLM output for first question
     next_question_id, llm_reply = start_survey()
 
     # Prepare the response data
-    response_data = {
-        'next_question_id': next_question_id,
-        'llm_reply': llm_reply
-    }
+    response_data = {"next_question_id": next_question_id, "llm_reply": llm_reply}
 
     # Return the response as JSON
     return jsonify(response_data)
 
 
 # Define a Flask route to handle POST requests
-@app.route('/get_question_id_and_llm_response', methods=['POST'])
+@app.route("/get_question_id_and_llm_response", methods=["POST"])
 def get_question_id_and_llm_response_api():
     # Extract user response from the request data
-    user_response = request.json.get('user_response')
-    stage = request.json.get('stage')
+    user_response = request.json.get("user_response")
+    stage = request.json.get("stage")
 
     # Call on function to get the response
     next_question_id, llm_reply = get_question_id_and_llm_response(user_response, stage)
 
     # Prepare the response data
-    response_data = {
-        'next_question_id': next_question_id,
-        'llm_reply': llm_reply
-    }
+    response_data = {"next_question_id": next_question_id, "llm_reply": llm_reply}
 
     # Return the response as JSON
     return jsonify(response_data)
 
-if __name__ == '__main__':
-    app.run(debug=False)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=False)

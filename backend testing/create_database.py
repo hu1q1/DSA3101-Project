30b556d6d8855f9d23071fe004a17eb3ba5dd8c3
kendrick <kendrickloi123@gmail.com>
2024-04-11 11:10:44 +0800

    minor edits

--- backend testing/create_database.py
+++ backend testing/create_database.py
@@ -29,29 +29,29 @@ def create_database():
     mycursor.execute("CREATE DATABASE IF NOT EXISTS testdatabase")
     mycursor.execute("USE testdatabase")
 
-    # create stage_0 table
-    mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS Stage_0(id INT PRIMARY KEY AUTO_INCREMENT, hair_length VARCHAR(50), hair_type VARCHAR(50), hair_concerns VARCHAR(100), scalp_type VARCHAR(100), scalp_concerns VARCHAR(100), hair_treatment VARCHAR(50))"
-    )
-
     # create stage_1 table
     mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS Stage_1(id INT PRIMARY KEY AUTO_INCREMENT, wash_frequency VARCHAR(50), hair_products VARCHAR(50), styling_products VARCHAR(50), prod_switch_freq VARCHAR(50), salon_freq VARCHAR(50), hair_goal VARCHAR(50), hair_health_importance VARCHAR(50))"
+        "CREATE TABLE IF NOT EXISTS Stage_1(id INT PRIMARY KEY AUTO_INCREMENT, hair_length VARCHAR(50), hair_type VARCHAR(50), hair_concerns VARCHAR(100), scalp_type VARCHAR(100), scalp_concerns VARCHAR(100), hair_treatment VARCHAR(50))"
     )
 
     # create stage_2 table
     mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS Stage_2(id INT PRIMARY KEY AUTO_INCREMENT, pantene_prod VARCHAR(50), pantene_info VARCHAR(50), most_fav_product VARCHAR(100), least_fav_product VARCHAR(100), prod_effectiveness VARCHAR(50), prod_recommend VARCHAR(50), desired_ingredients VARCHAR(50))"
+        "CREATE TABLE IF NOT EXISTS Stage_2(id INT PRIMARY KEY AUTO_INCREMENT, wash_frequency VARCHAR(50), hair_products VARCHAR(50), styling_products VARCHAR(50), prod_switch_freq VARCHAR(50), salon_freq VARCHAR(50), hair_goal VARCHAR(50), hair_health_importance VARCHAR(50))"
     )
 
     # create stage_3 table
     mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS Stage_3(id INT PRIMARY KEY AUTO_INCREMENT, important_factors VARCHAR(50), preferred_price_range VARCHAR(50), purchase_method VARCHAR(50))"
+        "CREATE TABLE IF NOT EXISTS Stage_3(id INT PRIMARY KEY AUTO_INCREMENT, pantene_prod VARCHAR(50), pantene_info VARCHAR(50), most_fav_product VARCHAR(100), least_fav_product VARCHAR(100), prod_effectiveness VARCHAR(50), prod_recommend VARCHAR(50), desired_ingredients VARCHAR(50))"
     )
 
-    # create demographic table
+    # create stage_4 table
     mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS Demographic(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), age VARCHAR(50), gender VARCHAR(50), stage0_id INT, stage1_id INT, stage2_id INT, stage3_id INT,FOREIGN KEY(stage0_id) REFERENCES Stage_0(id), FOREIGN KEY(stage1_id) REFERENCES Stage_1(id), FOREIGN KEY(stage2_id) REFERENCES Stage_2(id), FOREIGN KEY(stage3_id) REFERENCES Stage_3(id))"
+        "CREATE TABLE IF NOT EXISTS Stage_4(id INT PRIMARY KEY AUTO_INCREMENT, important_factors VARCHAR(50), preferred_price_range VARCHAR(50), purchase_method VARCHAR(50))"
+    )
+
+    # create stage_0 table
+    mycursor.execute(
+        "CREATE TABLE IF NOT EXISTS Stage_0(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), age VARCHAR(50), gender VARCHAR(50), stage1_id INT, stage2_id INT, stage3_id INT, stage4_id INT,FOREIGN KEY(stage1_id) REFERENCES Stage_1(id), FOREIGN KEY(stage2_id) REFERENCES Stage_2(id), FOREIGN KEY(stage3_id) REFERENCES Stage_3(id), FOREIGN KEY(stage4_id) REFERENCES Stage_4(id))"
     )
 
     db.commit()
@@ -84,9 +84,9 @@ def update_db(history):
     # add to database
     mycursor.execute("USE testdatabase")
 
-    # add to stage_0 table
+    # add to stage_1 table
     mycursor.execute(
-        "INSERT INTO Stage_0(hair_length, hair_type, hair_concerns, scalp_type, scalp_concerns, hair_treatment) VALUES (%s,%s,%s,%s,%s,%s)",
+        "INSERT INTO Stage_1(hair_length, hair_type, hair_concerns, scalp_type, scalp_concerns, hair_treatment) VALUES (%s,%s,%s,%s,%s,%s)",
         (
             get_r(history, 4),
             get_r(history, 5),
@@ -96,11 +96,11 @@ def update_db(history):
             get_r(history, 9),
         ),
     )
-    stage0_id = mycursor.lastrowid
+    stage1_id = mycursor.lastrowid
 
-    # add to stage_1 table
+    # add to stage_2 table
     mycursor.execute(
-        "INSERT INTO Stage_1(wash_frequency, hair_products, styling_products, prod_switch_freq, salon_freq, hair_goal, hair_health_importance) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+        "INSERT INTO Stage_2(wash_frequency, hair_products, styling_products, prod_switch_freq, salon_freq, hair_goal, hair_health_importance) VALUES (%s,%s,%s,%s,%s,%s,%s)",
         (
             get_r(history, 10),
             get_r(history, 11),
@@ -111,11 +111,11 @@ def update_db(history):
             get_r(history, 16),
         ),
     )
-    stage1_id = mycursor.lastrowid
+    stage2_id = mycursor.lastrowid
 
-    # add to stage_2 table
+    # add to stage_3 table
     mycursor.execute(
-        "INSERT INTO Stage_2(pantene_prod, pantene_info, most_fav_product, least_fav_product, prod_effectiveness, prod_recommend, desired_ingredients) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+        "INSERT INTO Stage_3(pantene_prod, pantene_info, most_fav_product, least_fav_product, prod_effectiveness, prod_recommend, desired_ingredients) VALUES (%s,%s,%s,%s,%s,%s,%s)",
         (
             get_r(history, 17),
             get_r(history, 18),
@@ -126,26 +126,26 @@ def update_db(history):
             get_r(history, 23),
         ),
     )
-    stage2_id = mycursor.lastrowid
-
-    # add to stage_3 table
-    mycursor.execute(
-        "INSERT INTO Stage_3(important_factors, preferred_price_range, purchase_method) VALUES (%s,%s,%s)",
-        (get_r(history, 24), get_r(history, 25), get_r(history, 26)),
-    )
     stage3_id = mycursor.lastrowid
 
-    # add to Demographic table
+    # add to stage_4 table
     mycursor.execute(
-        "INSERT INTO Demographic(name, age, gender, stage0_id, stage1_id, stage2_id, stage3_id) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+        "INSERT INTO Stage_4(important_factors, preferred_price_range, purchase_method) VALUES (%s,%s,%s)",
+        (get_r(history, 24), get_r(history, 25), get_r(history, 26)),
+    )
+    stage4_id = mycursor.lastrowid
+
+    # add to stage_0 table
+    mycursor.execute(
+        "INSERT INTO Stage_0(name, age, gender, stage0_id, stage1_id, stage2_id, stage3_id) VALUES (%s,%s,%s,%s,%s,%s,%s)",
         (
             get_r(history, 1),
             get_r(history, 2),
             get_r(history, 3),
-            stage0_id,
             stage1_id,
             stage2_id,
             stage3_id,
+            stage4_id,
         ),
     )
 
